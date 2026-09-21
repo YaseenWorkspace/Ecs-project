c522fca5ab1b76af4ceb2dbd5774cfcbb75b58aa
YaseenWorkspace <yaseena.ali@outlook.com>
2026-09-21 17:16:36 +0100

    Created a subnet command line to set up a subnet resource block

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -3,3 +3,7 @@ resource "aws_vpc" "ecs-vpc" {
     # The value of an IP address or IP address range.
     cidr_block = "10.0.0.0/16"
 }
+
+# This resource creates a subnet in the VPC for ECS.
+resource "aws_subnet" "ecs-subnet" {
+"
