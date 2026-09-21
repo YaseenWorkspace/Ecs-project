089de2cf124a1b3ac203089d71a97ffc7c38d9fd
YaseenWorkspace <yaseena.ali@outlook.com>
2026-09-21 19:20:26 +0100

    Created an internet gateway for the aws_vpc

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -8,3 +8,11 @@ resource "aws_vpc" "ecs-vpc" {
 resource "aws_subnet" "ecs-subnet" {
     # The ID of the VPC.
     vpc_id     = aws_vpc.ecs-vpc.id
+    # The value of an IP address or IP address range.
+    cidr_block = "10.0.1.0/24"
+}
+# This resource creates an internet gateway for the VPC.
+resource "internet_gateway" "ecs-igw" {
+    # The ID of the VPC.
+    vpc_id = aws_vpc.ecs-vpc.id
+}
