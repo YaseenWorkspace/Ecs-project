76a5c5aa347fb24a6cbc42871b25aa9eb810ea2d
YaseenWorkspace <yaseena.ali@outlook.com>
2026-09-21 17:14:24 +0100

    Added a cidr block in the vpc block

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -1,2 +1,5 @@
 # This module creates a VPC for ECS with the following resources:
 resource "aws_vpc" "ecs-vpc" {
+    # The value of an IP address or IP address range.
+    cidr_block = "10.0.0.0/16"
+}
