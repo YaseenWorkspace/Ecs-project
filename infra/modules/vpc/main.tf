ea97790b6fc0a3719ba838bf29e30d38bbd0b67a
YaseenWorkspace <yaseena.ali@outlook.com>
2026-09-21 21:14:46 +0100

    Added a resource block for a route table for my VPC

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -7,7 +7,6 @@ resource "aws_vpc" "ecs-vpc" {
         Name = "Project VPC"
     }
 }
-
 # This resource creates a subnet in the VPC for ECS.
 resource "aws_subnet" "private-subnet" {
     # The ID of the VPC.
@@ -20,14 +19,31 @@ resource "aws_subnet" "public-subnet" {
     vpc_id     = aws_vpc.ecs-vpc.id
     # The value of an IP address or IP address range.
     cidr_block = "10.0.2.0/24"
+    tags = {
+        # The name of the subnet.
+        Name = "Public Subnet"
+    }
 }
-
 # This resource creates an internet gateway for the VPC.
-resource "internet_gateway" "ecs-igw" {
+resource "aws_internet_gateway" "ecs-igw" {
     # The ID of the VPC.
     vpc_id = aws_vpc.ecs-vpc.id
     # The tags for the internet gateway.
     tags = {
         Name = "Project VPC IG"
     }
+}
+
+# This resource creates a route table for the VPC.
+resource "aws_route_table" "second_rt" {
+ vpc_id = aws_vpc.main.id
+ # The route table is associated with the VPC.
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.gw.id
+ }
+ # The tags for the route table.
+ tags = {
+   Name = "2nd Route Table"
+ }
 }
